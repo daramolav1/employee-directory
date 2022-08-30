43478de31d9e11d70fc84d7e08b4c0388192bb95
Victor <victorddaramola@gmail.com>
2022-08-30 18:58:28 -0400

    Imported table component and created function to filter list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import Table from "./components/Table";
 
 function App() {
+  const [employees, setEmployees] = useState([]);
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    async function loadEmployees() {
+      const response = await fetch("https://dummyjson.com/users");
+      const employeesFromAPI = await response.json();
+      setEmployees(employeesFromAPI.users);
+    }
+    loadEmployees();
+  }, []);
+
+  const keys = ["firstName", "lastName", "email"];
+
+  const search = (data) => {
+    return data.filter((item) =>
+      keys.some((key) => item[key].toLowerCase().includes(query))
+    );
+  };
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <input
+        type="text"
+        className="search"
+        placeholder="Search..."
+        onChange={({ target }) => setQuery(target.value)}
+      />
+      <Table employees={search(employees)} />
     </div>
   );
 }
